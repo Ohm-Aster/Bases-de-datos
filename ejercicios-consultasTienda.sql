aa27949ccbf3aeb6c70effd05f7d983a3e4ce11e
OhmAster <om_art1721@hotmail.com>
2022-03-30 07:59:37 -0600

    Add files via upload

---
+++ ejercicios-consultasTienda.sql
@@ -0,0 +1,24 @@
+/*
+1-Devuelve un listado de todos los fabricantes que existen en la base de datos, 
+junto con los productos que tiene cada uno de ellos. El listado deberá mostrar 
+también aquellos fabricantes que no tienen productos asociados.
+
+2-Devuelve un listado donde sólo aparezcan aquellos fabricantes que no tienen 
+ningún producto asociado.
+
+3-¿Pueden existir productos que no estén relacionados con un fabricante? Justifique su respuesta.
+
+4-Devuelve todos los productos del fabricante Lenovo. (Sin utilizar INNER JOIN).
+
+5-Lista el nombre del producto más caro del fabricante Lenovo.
+
+6-Lista el nombre del producto más barato del fabricante Hewlett-Packard.
+
+7-Calcula la suma de todos los productos del fabricante Asus.
+
+8-Calcula el número total de fabricantes que hay en la tabla fabricante.
+
+9-Lista los nombres de los fabricantes ordenados de forma ascendente.
+
+10-Lista los nombres de los fabricantes ordenados de forma descendente.
+*/
